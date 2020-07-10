8153e089b693504d79edf695915229900ce89c25
yasser <63634166+yasser-skiller@users.noreply.github.com>
2020-07-10 10:47:39 +0200

    Updates

---
+++ precache-manifest.6237e9c60e9eaa0289517a367263a6e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369386866e6d89869c8f0d515fed6ab5",
+    "url": "/yaser/index.html"
+  },
+  {
+    "revision": "ae8344429492ab5fd444",
+    "url": "/yaser/static/css/2.97d3883c.chunk.css"
+  },
+  {
+    "revision": "2127b0d7d91938e57f3a",
+    "url": "/yaser/static/css/main.fc29ecc5.chunk.css"
+  },
+  {
+    "revision": "ae8344429492ab5fd444",
+    "url": "/yaser/static/js/2.083f2a37.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/yaser/static/js/2.083f2a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2127b0d7d91938e57f3a",
+    "url": "/yaser/static/js/main.ebc3ef61.chunk.js"
+  },
+  {
+    "revision": "da97894716a7ce5b6ca0",
+    "url": "/yaser/static/js/runtime-main.ed310c5e.js"
+  },
+  {
+    "revision": "b62ecbfd04279dcc9a41fe1092166014",
+    "url": "/yaser/static/media/cairo.b62ecbfd.jpg"
+  },
+  {
+    "revision": "d1fad644d204ad654d65eca0cf16dcd2",
+    "url": "/yaser/static/media/images (1).d1fad644.jpg"
+  },
+  {
+    "revision": "b9c3d188d34ea71f3779172ec74a63eb",
+    "url": "/yaser/static/media/logo.b9c3d188.png"
+  },
+  {
+    "revision": "61ee5be15f7a8e7794f5d0e06335c925",
+    "url": "/yaser/static/media/photos_صورة_عالية_الوضوح_للكعبة_20170830261656.61ee5be1.jpg"
+  },
+  {
+    "revision": "c185ce1b75e5c63aac5b5bd5a17970d0",
+    "url": "/yaser/static/media/statue-of-liberty-1210001_1920.c185ce1b.jpg"
+  }
+]);
